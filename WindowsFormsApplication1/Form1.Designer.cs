27688729f66b12a3e9c4f9e0608b5af5c163df67
david_000 <davidbugayov@ya.ru>
2017-03-21 02:26:23 +0300

    добавил сохранение файл

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -51,12 +51,14 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Открыть";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
+            //this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
             // button2
             // 
@@ -66,8 +68,9 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Сохранить";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // chart1
             // 
@@ -78,6 +81,7 @@
             this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(0, 0);
             this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
             series1.Name = "Series1";
@@ -85,6 +89,7 @@
             this.chart1.Size = new System.Drawing.Size(626, 300);
             this.chart1.TabIndex = 2;
             this.chart1.Text = "chart1";
+            this.chart1.UseWaitCursor = true;
             // 
             // Form1
             // 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,5 +18,59 @@ namespace WindowsFormsApplication1
 
             InitializeComponent();
         }
+        List<String> list = new List<string>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Displays an OpenFileDialog so the user can select a Cursor.
+   
+            openFileDialog1.Filter = "Текстовый файл|*.txt|Любой файл(*.*)|*.*";
+            openFileDialog1.Title = "Выберите текстовый файл";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.FileName = "";
+            try
+                {
+                    if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        System.IO.StreamReader sr = new
+                           System.IO.StreamReader(openFileDialog1.FileName);
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        list.Add(line);
+                        Console.WriteLine(line);
+                    }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("ОШИБКА, НЕВОЗМОЖНО ОТКРЫТЬ ФАЙЛ: " + ex.Message);
+                }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (list.Count != 0)
+            {
+                String fileName = "Чистый" + openFileDialog1.SafeFileName;
+                TextWriter tw = new StreamWriter(fileName);
+                MessageBox.Show(fileName);
+                saveFileDialog1.Filter = "Текстовый файл | *.txt";
+                saveFileDialog1.DefaultExt = "txt";
+                saveFileDialog1.FileName = fileName;
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    StreamWriter writer = new StreamWriter(saveFileDialog1.OpenFile());
+                       foreach(String li in list)
+                        writer.WriteLine(li);
+                    writer.Dispose();
+                    writer.Close();
+                }
+                 }else {
+                MessageBox.Show("Выберите файл");
+            }
+        }
+
     }
 }
